61655e2e210fd25bd86d6ae49ac6ddb97d8c67fc
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2019-07-24 16:14:31 -0700

    abc

--- R/agrin-data.R
+++ R/agrin-data.R
@@ -4,16 +4,76 @@
 # license GPL3
 
 
+.setExtension <- function(filename, value) {
+	value <- trimws(value)
+	if (value != "" & substr(value, 1, 1) != ".") {
+		value <- paste(".", value, sep="") 
+	}
+	lfn <- nchar(filename)
+	fname <- list()
+	for (f in 1:length(filename)) {
+		extstart <- -1
+		for (i in lfn[f] : 2) {
+			if (substr(filename[f], i, i) == ".") {
+				extstart <- i
+				break 
+			}
+		}
+		if (extstart > 0 & (lfn[f] - extstart) < 8) {
+			fname[f] <- paste(substr(filename[f], 1, extstart-1), value, sep="")
+		} else { 
+			fname[f] <- paste(filename[f], value, sep="")  
+		}
+	}
+	return( unlist(fname) ) 
+}   
+
+
+
+.extension <- function(filename, value=NULL, maxchar=10) {
+	if (!is.null(value)) {
+		filename <- .setExtension(filename, value)
+		return(filename)
+	}   
+	lfn <- nchar(filename)
+	ext <- list()
+	for (f in 1:length(filename)) {
+		extstart <- -1
+		for (i in lfn[f] : 2) {
+			if (substr(filename[f], i, i) == ".") {
+				extstart <- i
+				break
+			}
+		}
+		if (extstart > 0) {
+			ext[f] <- substr(filename[f], extstart, lfn[f])
+		} else { 
+			ext[f] <- "" 
+		}   
+	}
+	ext <- unlist(ext)
+	ext[nchar(ext) > maxchar] <- ''
+	return(ext)
+}   
+
+
 agrin_data <- function(name) {
 	
-	name <- name[1]
-	raster::extension(name) <- '.rds'
+	name <- tolower(name[1])
+	n <- nchar(name)
+	sp <- substr(name, n-1, n) == "sp"
+	name <- .setExtension(name, ".rds")
 	fn <- system.file(file.path("rds", name), package="agrin")
 	if (!(file.exists(fn))) {
-		stop(paste(name, 'is not a valid data set name.'))
+		stop(paste(name, "is not a valid data set name"))
 	}
-	readRDS(fn)
-	
+	x <- readRDS(fn)
+	if (sp) {
+		return(x)
+	} else {
+		y <- vect(x$geom, type=x$type, atts=x$df, crs=x$crs)
+		return(y)
+	}
 }
 
 
